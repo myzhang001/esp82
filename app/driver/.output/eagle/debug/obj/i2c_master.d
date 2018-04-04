b6118378b47a9fc8115fb24035d0d9b19d545f71
zhangmuyang <279401783@qq.com>
2018-04-04 11:35:46 +0800

    first commit

---
+++ app/driver/.output/eagle/debug/obj/i2c_master.d
@@ -0,0 +1,18 @@
+.output/eagle/debug/obj/i2c_master.o .output/eagle/debug/obj/i2c_master.d : i2c_master.c \
+ /cygdrive/d/ESP8266/code/ESP8266_RTOS_SDK-2.0.0/include/espressif/c_types.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stdint.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/stdint.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/newlib.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/config.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/ieeefp.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/features.h \
+ /cygdrive/d/ESP8266/code/ESP8266_RTOS_SDK-2.0.0/extra_include/xtensa/config/core-isa.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stdbool.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stddef.h \
+ /cygdrive/d/ESP8266/code/ESP8266_RTOS_SDK-2.0.0/include/espressif/esp8266/ets_sys.h \
+ ../include/gpio.h \
+ /cygdrive/d/ESP8266/code/ESP8266_RTOS_SDK-2.0.0/include/espressif/esp8266/gpio_register.h \
+ ../include/i2c_master.h \
+ /cygdrive/d/ESP8266/code/ESP8266_RTOS_SDK-2.0.0/include/espressif/esp8266/pin_mux_register.h \
+ /cygdrive/d/ESP8266/code/ESP8266_RTOS_SDK-2.0.0/include/espressif/esp8266/eagle_soc.h
---
+++ app/driver/.output/eagle/debug/obj/spi_interface.d
@@ -0,0 +1,18 @@
+.output/eagle/debug/obj/spi_interface.o .output/eagle/debug/obj/spi_interface.d : spi_interface.c ../include/spi_interface.h \
+ ../include/spi_register.h \
+ /cygdrive/d/ESP8266/code/ESP8266_RTOS_SDK-2.0.0/include/espressif/c_types.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stdint.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/stdint.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/newlib.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/config.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/ieeefp.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/features.h \
+ /cygdrive/d/ESP8266/code/ESP8266_RTOS_SDK-2.0.0/extra_include/xtensa/config/core-isa.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stdbool.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stddef.h \
+ /cygdrive/d/ESP8266/code/ESP8266_RTOS_SDK-2.0.0/include/espressif/esp8266/eagle_soc.h \
+ /cygdrive/d/ESP8266/code/ESP8266_RTOS_SDK-2.0.0/include/espressif/esp8266/ets_sys.h \
+ /cygdrive/d/ESP8266/code/ESP8266_RTOS_SDK-2.0.0/include/espressif/esp8266/pin_mux_register.h \
+ /cygdrive/d/ESP8266/code/ESP8266_RTOS_SDK-2.0.0/include/espressif/esp8266/eagle_soc.h \
+ /cygdrive/d/ESP8266/code/ESP8266_RTOS_SDK-2.0.0/include/espressif/esp_libc.h
